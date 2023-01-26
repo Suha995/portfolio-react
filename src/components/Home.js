048a66f997a42a759eb3b7b25c56173295e146cb
Suhair <saljammal995@gmail.com>
2023-01-26 14:44:31 +0200

    build the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,30 @@
+import pPhoto from "../imgs/personal-photo.png";
+import { Link } from "react-router-dom";
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div className="icons">
+        <a href="https://github.com/Suha995" target="_blank">
+          <i class="fa-brands fa-github fa-3x"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/suhair-aljammal/" target="_blank">
+          <i class="fa-brands fa-linkedin fa-3x"></i>
+        </a>
+      </div>
+      <div className="info">
+        <div className="info-left">
+          <h2>Suhair Aljammal</h2>
+          <p>
+            I'm web devloper. Seeking a position in front-end web develpoment to
+            challenge and improve myself.
+          </p>
+        </div>
+        <div className="info-right">
+          <img src={pPhoto} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
