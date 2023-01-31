491ff7bea5204bce5e81ae3806cb87bc988612e5
Suhair <saljammal995@gmail.com>
2023-01-31 16:40:27 +0200

    fix the sidebar and navbar to be more responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,20 +3,25 @@ import { Link } from "react-router-dom";
 import "./navbar.scss";
 
 const Navbar = () => {
-  const handleClick = () => {
-    console.log("click");
+  const handleClickBurger = () => {
     document.getElementsByClassName("sidebar")[0].style.display = "flex";
+  };
+  const handleClickClose = () => {
+    document.getElementsByClassName("sidebar")[0].style.display = "none";
   };
   return (
     <div className="navbar">
       <div className="container">
         <div className="logo">Suhair</div>
-        <div className="burger" onClick={handleClick}>
+        <div className="burger" onClick={handleClickBurger}>
           <div className="line"></div>
           <div className="line"></div>
           <div className="line"></div>
         </div>
         <div className="sidebar">
+          <span className="close-tab" onClick={handleClickClose}>
+            &times;
+          </span>
           <Link to={"/"}>HOME</Link>
           <Link to={"/skills"}>SKILLS</Link>
           <Link to={"/Projects"}>PROJECTS</Link>
